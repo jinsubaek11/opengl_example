4bf361ab622c85f22968e6f2d79e9282c5e0ad35
jinsubaek <bjinsu93@gmail.com>
2022-09-02 21:49:51 +0900

    feat: update shader source

--- shader/simple.fs
+++ shader/simple.fs
@@ -1,14 +1,9 @@
 #version 330 core
 
 uniform vec4 color;
-uniform sampler2D tex;
-uniform sampler2D tex2;
-
-in vec2 texCoord;
-
 out vec4 fragColor;
 
 void main()
 {
-    fragColor = texture(tex, texCoord) * 0.8 + texture(tex2, texCoord) * 0.2;
+    fragColor = color;
 }
